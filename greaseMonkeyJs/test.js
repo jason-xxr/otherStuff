6b50b0eea826ae932bd2e8bc63b5a439778e6a91
jason-xxr <jason.xxr@gmail.com>
2019-03-02 18:11:52 -0800

    Create test.js

---
+++ greaseMonkeyJs/test.js
@@ -0,0 +1,16 @@
+
+// ==UserScript==
+// @name          prodScreenshot
+// @description   Add the corresponding thumbnail inside each link to a prod.
+// @include http://pouet.net/*
+// @exclude http://pouet.net/
+// @exclude http://pouet.net/index.php*
+// @include http://www.pouet.net/*
+// @exclude http://www.pouet.net/
+// @exclude http://www.pouet.net/index.php*
+// ==/UserScript==
+/*
+ * 2012.08.24 Tigrou^ind ( tigrou.ind@gmail.com ). Original idea and implementation
+ * 2012.08.24 Mathieu 'p01' Henri ( http://www.p01.org/ ). Rewrite working on ALL pages excluding the index which would get too busy.
+ */
+!(function (){})();
