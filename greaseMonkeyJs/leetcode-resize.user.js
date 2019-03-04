e545114ae34206abe8d982280d89727cb2850f0f
jason-xxr <jason.xxr@gmail.com>
2019-03-03 21:57:35 -0800

    Update leetcode-resize.user.js

--- greaseMonkeyJs/leetcode-resize.user.js
+++ greaseMonkeyJs/leetcode-resize.user.js
@@ -6,22 +6,31 @@
 // @author       You
 // @match        https://leetcode.com/problems/*
 // @grant        none
+// @require      https://raw.githubusercontent.com/mlcheng/js-toast/master/toast.js
 // ==/UserScript==
 
 (function() {
     'use strict';
     var maxInputHeight = "200px";
     var maxPanelHeight = "450px";
+    var toast = new iqwerty.toast.Toast();
 
     setTimeout(function(){
         var consolePanel = document.getElementsByClassName("custom-testcase__2ah7");
         console.log("consolePanel.length", consolePanel.length);
+        
         if (consolePanel.length>0) {
+            toast.setText('Console panel captured.')
+                .setDuration(1000)
+                .show();
             consolePanel[0].addEventListener("click", function(){
                 setTimeout(function(){
                     var resultTab = document.getElementsByClassName("css-106l9hg-TabHeader e5i1odf4");
                     console.log("resultTab.length", resultTab.length);
                     if (resultTab.length>0) {
+                        toast.setText('Activated.')
+                            .setDuration(500)
+                            .show();
                         resultTab[0].addEventListener("click", function(){
                             var resultPanel = document.getElementsByClassName("css-187w515-TabContent e5i1odf5");
                             console.log("resultPanel.length", resultPanel.length);
@@ -37,9 +46,17 @@
                             }
                             console.log("resized");
                         });
+                    } else {
+                        toast.setText('Result tab not found, please reload.')
+                            .setDuration(1000)
+                            .show();
                     }
                 }, 1000);
             })
+        } else {
+            toast.setText('Console panel not found, please reload')
+                .setDuration(3000)
+                .show();
         }
-    }, 4000);
+    }, 5000);
 })();
