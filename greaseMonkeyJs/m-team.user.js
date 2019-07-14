ae945a3e61a2f10f38644b083c629798295e5a27
jason-xxr <jason.xxr@gmail.com>
2019-07-13 21:49:16 -0700

    Update m-team.user.js

--- greaseMonkeyJs/m-team.user.js
+++ greaseMonkeyJs/m-team.user.js
@@ -1,20 +1,21 @@
 // ==UserScript==
-// @name         TP Seeders 120+
+// @name         TP Seeders 100+
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://tp.m-team.cc/torrents.php*
+// @match        https://pt.m-team.cc/torrents.php*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var threshold = 60;
+
+    var threshold = 50;
     var table = document.getElementById("form_torrent").getElementsByClassName("torrents")[0];
     for (var i = 1, row; row = table.rows[i]; i++) {
         var sd = row.cells[5].getElementsByTagName("a")[0];
-        if (typeof sd === "undefined" || parseInt(sd.text) < threshold) {
+        if (typeof sd === "undefined" || parseInt(sd.text.replace(/,/g, "")) < threshold) {
             row.style.display = 'none';
         }
     }
